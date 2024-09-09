7c98a5dcd4a1b5148bf9142fa1509c0b663a9046
gmovia <gmovia@fi.uba.ar>
2024-09-09 17:05:36 -0300

    feat: change time from seconds to minutes

--- src/common/services/jwt/mappers/jwt_mapper.py
+++ src/common/services/jwt/mappers/jwt_mapper.py
@@ -5,6 +5,6 @@ def map_to_payload(message, expiration_time):
     return {
         "iat": datetime.datetime.now(datetime.UTC),
         "exp": datetime.datetime.now(datetime.UTC)
-        + datetime.timedelta(seconds=expiration_time),
+        + datetime.timedelta(minutes=expiration_time),
         "sub": message,
     }
