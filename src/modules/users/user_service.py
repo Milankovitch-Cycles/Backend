599f33c5281d95f79d3c0f2e4184023069bb4de1
gmovia <gmovia@fi.uba.ar>
2024-09-09 17:06:39 -0300

    feat: add update user function

--- src/modules/users/user_service.py
+++ src/modules/users/user_service.py
@@ -6,7 +6,7 @@ class UserService:
     def __init__(self, session: Session):
         self.session = session
 
-    def create(self, email: str, password: str) -> UserEntity:
+    def create_user(self, email: str, password: str) -> UserEntity:
         user = UserEntity(email=email, password=password)
         self.session.add(user)
         self.session.commit()
@@ -14,3 +14,11 @@ class UserService:
 
     def get_by_email(self, email: str) -> UserEntity:
         return self.session.query(UserEntity).filter(UserEntity.email == email).first()
+
+    def update_user(self, email: str, new_attributes: dict) -> UserEntity:
+        user = self.get_by_email(email)
+        for attr, value in new_attributes.items():
+            setattr(user, attr, value)
+        self.session.add(user)
+        self.session.commit()
+        return user
