8714c055451b0af9fd28e527ea94e025a6c49afa
clezious <manuelreberendo@gmail.com>
2024-10-06 22:58:48 -0300

    cors fix for local development

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.modules.auth.auth_routes import auth
 from src.modules.wells.well_routes import wells
 
 app = FastAPI(title="Milankovic API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth)
 app.include_router(wells)
