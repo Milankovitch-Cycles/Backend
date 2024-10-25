de48a96569bcdc0a55bbdb90f5e40a164d1a1119
gmovia <gmovia@fi.uba.ar>
2024-10-24 22:33:03 -0300

    improvemment: scatter plot

--- worker/src/plots/plot/strategies/scatter.py
+++ worker/src/plots/plot/strategies/scatter.py
@@ -1,5 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 from pandas import DataFrame
-from matplotlib import pyplot as plt
 from src.plots.plot.mappers.mappers import map_code_to_description
 from src.plots.plot.strategies.strategy import PlotStrategy
 
@@ -9,10 +10,16 @@ class Scatter(PlotStrategy):
         super().__init__(path=f"{map_code_to_description(self.column).replace(' ', '-')}/scatter.png")
     
     def plot(self, dataframe: DataFrame):
-        plt.scatter(dataframe.index, dataframe[self.column], marker="o", alpha=0.4)
+        plt.figure(figsize=(10, 6))
         
+        sns.scatterplot(x=dataframe.index, y=dataframe[self.column], 
+                        marker="o", alpha=0.5, s=80, color="dodgerblue", 
+                        edgecolor=None, linewidth=0.5)
+                
         x_label = map_code_to_description("DEPT")
         y_label = map_code_to_description(self.column)
+        
+        plt.title(f"Scatter Plot of {y_label}", fontsize=16)
         
         self.set_labels(x_label, y_label)
         
