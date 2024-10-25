60d34100932c197ee868b868336607e783d394ed
gmovia <gmovia@fi.uba.ar>
2024-10-24 22:33:23 -0300

    improvemment: depth plot

--- worker/src/plots/plot/strategies/depth.py
+++ worker/src/plots/plot/strategies/depth.py
@@ -10,10 +10,14 @@ class Depth(PlotStrategy):
         super().__init__(path=f"{map_code_to_description(self.column).replace(' ', '-')}/depth.png")
     
     def plot(self, dataframe: DataFrame):
-        plt.plot(dataframe[self.column], dataframe.index, color = "blue")
+        plt.figure(figsize=(10, 6))
+
+        plt.plot(dataframe[self.column], dataframe.index, color = "dodgerblue")
         plt.gca().invert_yaxis()
        
         x_label = map_code_to_description(self.column)
         y_label = map_code_to_description("DEPT")
+        
+        plt.title(f"Depth versus {x_label} chart", fontsize=16)    
         
         self.set_labels(x_label, y_label)        
