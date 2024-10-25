ee8863d0274a23eb7d43992b2f8e9ea2e1de9afa
gmovia <gmovia@fi.uba.ar>
2024-10-24 22:33:17 -0300

    improvemment: histogram plot

--- worker/src/plots/plot/strategies/histogram.py
+++ worker/src/plots/plot/strategies/histogram.py
@@ -1,3 +1,4 @@
+import seaborn as sns
 from pandas import DataFrame
 from matplotlib import pyplot as plt
 from src.plots.plot.mappers.mappers import map_code_to_description
@@ -10,10 +11,15 @@ class Histogram(PlotStrategy):
         super().__init__(path=f"{map_code_to_description(column).replace(' ', '-')}/histogram.png")
     
     def plot(self, dataframe: DataFrame):
-        plt.hist(dataframe[self.column], bins=30, color="blue", edgecolor="black", alpha=0.5)
-        
+        sns.set_theme(style="whitegrid")
+
+        plt.figure(figsize=(10, 6))
+        sns.histplot(dataframe[self.column], bins=30, kde=True, color="dodgerblue", edgecolor="black", alpha=0.7)
+
         x_label = map_code_to_description(self.column)
         y_label = map_code_to_description("FREQ")
 
+        plt.title(f"{x_label} Histogram", fontsize=16)    
+        
         self.set_labels(x_label, y_label)
         
