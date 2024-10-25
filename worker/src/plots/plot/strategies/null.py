9329ef1789bbd7baa5c21dc331f0bb269567d7e3
gmovia <gmovia@fi.uba.ar>
2024-10-24 22:33:10 -0300

    improvemment: null plot

--- worker/src/plots/plot/strategies/null.py
+++ worker/src/plots/plot/strategies/null.py
@@ -4,7 +4,7 @@ from src.plots.plot.strategies.strategy import PlotStrategy
 
 class Null(PlotStrategy):
     def __init__(self):
-        super().__init__(path="/null.png")
+        super().__init__(path="null.png")
     
     def plot(self, dataframe: DataFrame):
         mnso.bar(dataframe)
