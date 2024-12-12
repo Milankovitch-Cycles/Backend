c5fc3df568be7659c2fc3ab06449a5882b1ef8cc
gmovia <gmovia@fi.uba.ar>
2024-12-12 20:29:57 -0300

    hotfix: process gamma ray

--- worker/src/processor/new_well_processor.py
+++ worker/src/processor/new_well_processor.py
@@ -1,22 +1,24 @@
 import os
+import pandas as pd
 from pandas import DataFrame
 from src.processor.job_processor import JobProcessor
 
-        
 class NewWellProcessor(JobProcessor):
     def write_csv(self, dataframe: DataFrame, path: str):
         if not os.path.exists(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))       
-        numeric_dataframe = dataframe.select_dtypes(include=['int'])
-        variation = numeric_dataframe.diff().abs().sum(axis=1)
-        representative_dataframe = dataframe.loc[variation.nlargest(200).index]
-        representative_dataframe.to_csv(path, float_format='%.0f')
+        dataframe.to_csv(path, float_format='%.2f', index=False)
         return path
-    
-    def process(self, job, dataframe):
-        gamma_ray_dataframe = dataframe[['GR']].copy()
-        gamma_ray_dataframe['TEMP_DEPTH'] = dataframe.index.astype(int)
-        gamma_ray_dataframe['GR'] = dataframe['GR'].dropna().astype(int)
-        gamma_ray_dataframe = gamma_ray_dataframe.groupby('TEMP_DEPTH').mean()
-        gamma_ray_path = self.write_csv(gamma_ray_dataframe[["GR"]], f"./static/{job.well_id}/gamma_ray.txt")
+
+    def process(self, job, dataframe, step = 20):
+        gamma_ray_dataframe = dataframe[['GR']].dropna().copy()
+        gamma_ray_dataframe['TEMP_DEPTH'] = gamma_ray_dataframe.index.astype(int)
+        gamma_ray_dataframe['GROUP'] = (gamma_ray_dataframe['TEMP_DEPTH'] // step).astype(int)
+        averaged_dataframe = gamma_ray_dataframe.groupby('GROUP').apply(
+            lambda group: {
+                'TEMP_DEPTH': int(group['TEMP_DEPTH'].iloc[0]),
+                'GR': int(group['GR'].mean())
+            }
+        ).apply(pd.Series).reset_index(drop=True)
+        gamma_ray_path = self.write_csv(averaged_dataframe, f"./static/{job.well_id}/gamma_ray.txt")
         return {"gamma_ray_path": gamma_ray_path}
